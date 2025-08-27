7a33a913952634c26082a37223dff314f33cb8a3
Daniel Vigano <danielvigano17@gmail.com>
2025-08-27 00:38:46 -0300

    style: ajuste visual tela de geração de questionário

--- src/app/(app)/(quiz)/quiz/_components/quiz-component.tsx
+++ src/app/(app)/(quiz)/quiz/_components/quiz-component.tsx
@@ -85,7 +85,7 @@ export function QuizApp() {
   }
 
   return (
-    <div className="container max-h-screen h-full overflow-y-auto mx-auto pb-24 py-8 px-4">
+    <div className="container h-fit overflow-y-auto mx-auto pb-24 py-8">
       <AnimatePresence mode="wait">
         {generatedQuestions ? (
           // Tela do formulário de perguntas gerado
