91e0477b941ab9cc8bc4e4c42333765390f60df6
Daniel Vigano <danielvigano17@gmail.com>
2025-10-26 14:56:05 -0300

    fix: remove por robots.txt a tela de obrigado

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -17,6 +17,7 @@ export default function robots(): MetadataRoute.Robots {
         disallow: [
           '/api/*',
           '/app/*',
+          '/obrigado/*',
           '/billing/*',
           '/on-boarding/*',
           '/quiz/*',
